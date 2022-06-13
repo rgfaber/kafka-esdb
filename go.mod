a706a1f0ee6a7e61f678eb91ad8b59a17a5f88bb
Rafael Lefever <rafael.lefever@discomco.pl>
2022-06-13 10:57:03 +0200

    initial commit

---
+++ go.mod
@@ -0,0 +1,38 @@
+module github.com/rgfaber/kafka-esdb
+
+go 1.18
+
+require (
+	github.com/EventStore/EventStore-Client-Go v1.0.2
+	github.com/labstack/echo/v4 v4.7.2
+	github.com/opentracing/opentracing-go v1.2.0
+	github.com/pkg/errors v0.9.1
+	github.com/satori/go.uuid v1.2.0
+	github.com/uber/jaeger-client-go v2.30.0+incompatible
+	go.uber.org/zap v1.21.0
+	google.golang.org/grpc v1.47.0
+)
+
+require (
+	github.com/HdrHistogram/hdrhistogram-go v1.1.2 // indirect
+	github.com/gofrs/uuid v4.2.0+incompatible // indirect
+	github.com/golang/mock v1.6.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/labstack/gommon v0.3.1 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
+	github.com/uber/jaeger-lib v2.4.1+incompatible // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.1 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.7.0 // indirect
+	golang.org/x/crypto v0.0.0-20220131195533-30dcbda58838 // indirect
+	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
+	golang.org/x/sys v0.0.0-20220204135822-1c1b9b1eba6a // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/genproto v0.0.0-20220204002441-d6cc3cc0770e // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
---
+++ pkg/common/interfaces.go
@@ -0,0 +1,69 @@
+package common
+
+import (
+	"context"
+)
+
+type Snapshot struct {
+	ID      string        `json:"id"`
+	Type    AggregateType `json:"type"`
+	State   []byte        `json:"state"`
+	Version uint64        `json:"version"`
+}
+
+type ILoad interface {
+	Loads(events []Event) error
+}
+
+type IApply interface {
+	Apply(evt Event) error
+}
+
+type IAggregateRoot interface {
+	GetUncommittedEvents() []Event
+	GetID() string
+	SetID(id string) *AggregateBase
+	GetVersion() int64
+	ClearUncommittedEvents()
+	ToSnapshot()
+	SetType(aggregateType AggregateType)
+	GetType() AggregateType
+	SetAppliedEvents(events []Event)
+	GetAppliedEvents() []Event
+	RaiseEvent(event Event) error
+	String() string
+}
+
+type IAggregate interface {
+	IAggregateRoot
+	When(event Event) error
+}
+
+type IAggregateStore interface {
+	// Load loads the most recent version of an aggregate to provided  into params aggregate with a type and id.
+	Load(ctx context.Context, aggregate IAggregate) error
+	// Save saves the uncommitted events for an aggregate.
+	Save(ctx context.Context, aggregate IAggregate) error
+	// Exists check aggregate exists by id.
+	Exists(ctx context.Context, streamID string) error
+	// IEventStore
+	// ISnapshotStore
+}
+
+// EventStore is an interface for an event sourcing event store.
+type IEventStore interface {
+	// SaveEvents appends all events in the event stream to the store.
+	SaveEvents(ctx context.Context, streamID string, events []Event) error
+
+	// LoadEvents loads all events for the aggregate id from the store.
+	LoadEvents(ctx context.Context, streamID string) ([]Event, error)
+}
+
+// ISnapshotStore is an interface for an event sourcing snapshot store.
+type ISnapshotStore interface {
+	// SaveSnapshot save aggregate snapshot.
+	SaveSnapshot(ctx context.Context, aggregate IAggregate) error
+
+	// GetSnapshot load aggregate snapshot.
+	GetSnapshot(ctx context.Context, id string) (*Snapshot, error)
+}
